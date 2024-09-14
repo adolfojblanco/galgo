be8287a798b405048694e99d5ead8ed58a59861f
Adolfo Blanco <ajb156@outlook.com>
2024-09-14 15:08:47 +0200

    update

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { provideHotToastConfig } from '@ngneat/hot-toast';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MaterialModule } from './components/shared/material/material.module';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './interceptors/auth.interceptor';
+import { provideHotToastConfig } from '@ngxpert/hot-toast';
 
 @NgModule({
   declarations: [
